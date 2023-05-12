9bbc7437c8928e4e48d535735c2356c750a618f2
tiger <375478250@qq.com>
2023-05-12 10:43:43 +0800

    接收命令json化

--- main.cpp
+++ main.cpp
@@ -8,32 +8,51 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    std::cout << std::setw(4) << json::meta() << std::endl;
+    //    std::cout << std::setw(4) << json::meta() << std::endl;
 
     XNode node;
 
-//    if(argc > 2) {
-//        thread server_thread(&XNode::service, &node, 8888);
-//        server_thread.detach();
-//    } else {
-        thread server_thread(&XNode::service, &node, atoi(argv[1]));
+
+    if(string(argv[1]) == "listen") {
+        thread server_thread(&XNode::service, &node, atoi(argv[2]));
         server_thread.detach();
+    }
+
+
+    string cmd = R"(
+        {
+        "group": [
+            {"ip":"127.0.0.1", "port": 10000},
+            {"ip":"127.0.0.1", "port": 10001},
+            {"ip":"127.0.0.1", "port": 10002}
+        ],
+            "times": 10,
+            "interval": 1000,
+            "content": "hello world"
+        }
+    )";
+
+//    cout<<json::parse(cmd).dump(4)<<endl;
+    auto c = json::parse(cmd);
+
+    cout<<c["content"]<<endl;
+    for(auto group : c["group"]) {
+        cout<<group["ip"]<<"\t"<<group["port"]<<endl;
+    }
+
+
+
+
+//    if(argc > 2 && argc <= 4) {
+//        thread client_thread(&XNode::contact, &node, argv[2], atoi(argv[3]));
+//        client_thread.detach();
 //    }
 
 
-
-
-
-    if(argc > 2 && argc <= 4) {
-        thread client_thread(&XNode::contact, &node, argv[2], atoi(argv[3]));
-        client_thread.detach();
-    }
-
-
-    if(argc > 4 && argc <= 6) {
-        thread client_thread(&XNode::contact, &node, argv[4], atoi(argv[5]));
-        client_thread.detach();
-    }
+//    if(argc > 4 && argc <= 6) {
+//        thread client_thread(&XNode::contact, &node, argv[4], atoi(argv[5]));
+//        client_thread.detach();
+//    }
 
 
     //    for(const auto &client : node.clients) {
